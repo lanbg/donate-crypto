76e7f7cdc23a3cc6c4316d5c982693696b5e3431
lanbg <laniabg@yahoo.com.br>
2025-10-30 16:34:06 -0300

    alter

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,52 @@
-import Image from "next/image";
+"use client"
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { doLogin } from "@/services/Web3Service";
 
 export default function Home() {
+
+  const { push } = useRouter();
+
+  const [message, setMessage] = useState("");
+
+  function btnLoginClick() {
+    setMessage("Conectando na carteira...aguarde...");
+    doLogin()
+      .then(account => push("/create"))
+      .catch(err => {
+        console.error(err);
+        setMessage(err.message);
+      })
+
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <div className="container px-4 py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div className="col-6">
+            <img src="https://images.unsplash.com/photo-1520694478166-daaaaec95b69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80" className="d-block mx-lg-auto img-fluid" width="700" height="500" />
+          </div>
+          <div className="col-6">
+            <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Donate Crypto</h1>
+            <p className="lead">Sua plataforma descentralizada de doações.</p>
+            <p className="lead">Autentique-se com sua carteira e crie sua campanha.</p>
+            <p className="lead mb-3">Para doações, use o link da campanha já existente.</p>
+            <div className="d-flex justify-content-start mt-5">
+              <button type="button" className="btn btn-primary btn-lg px-4 me-2 col-12" onClick={btnLoginClick}>
+                <img src="/metamask.svg" width="64" className="me-2" />
+                Conectar com a MetaMask
+              </button>
+            </div>
+            {
+              message
+                ? <div className="alert alert-success p-3 col-12 mt-3" role="alert">{message}</div>
+                : <></>
+            }
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </div>
+    </>
   );
 }
